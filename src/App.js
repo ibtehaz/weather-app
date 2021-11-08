fb3832ea1a66bd0672e1230d63583fa95c063a47
ibtehaz <ibtehaz.utsay@gmail.com>
2021-11-08 14:40:30 -0800

    fetch data using axios and display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [allData, setAllData] = useState({
+    city: "",
+    country: "",
+    temp: "",
+  });
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async (city) => {
+    const api_key = "b55547b292b3f8ba9ec9370d2107d604";
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${'london'}&appid=${api_key}&units=metric`;
+    const response = await axios.get(url);
+    const data = response.data;
+    setAllData({
+      city: data.name,
+      country: data.sys.country,
+      temp: data.main.temp,
+    });
+  };
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello {allData.country}</h1>
+        <h1>Hello {allData.temp}℉</h1>
       </header>
     </div>
   );
-}
+
+  }
+
+// const fetchData = async (city) => {
+//   const API_KEY = "b55547b292b3f8ba9ec9370d2107d604";
+//   const result = await axios.get(
+//     `http://api.openweathermap.org/data/2.5/weather?q=${"milan"}&appid=${API_KEY}`
+//   );
+//   await setAllData({
+//     city: result.data.name,
+//     country: result.data.sys.country,
+//     temp: result.data.main.temp,
+//   });
+// };
+  
+
 
 export default App;
